b12393dd6b1c070cacf61778576ce0875313bd5d
woojin <tkdalsrb147@naver.com>
2024-12-30 15:34:59 +0900

    Initial commit

---
+++ src/main/java/zerobase/tablemate/TableMateApplication.java
@@ -0,0 +1,13 @@
+package zerobase.tablemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/tablemate/TableMateApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.tablemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
